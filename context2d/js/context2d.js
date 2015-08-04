6983abbe956f14f502978d7559621d7ca0fe4dbf
ics-kano <takeshi.kano@ics-web.jp>
2015-08-04 10:26:05 +0900

    HTML名をリネーム

--- context2d/js/context2d.js
+++ context2d/js/context2d.js
@@ -1,9 +1,16 @@
+/// <reference path="../../dts/easeljs/easeljs.d.ts" />
+/// <reference path="../../dts/preloadjs/preloadjs.d.ts" />
 /// <reference path="../../common/js/FPSChecker.ts" />
+var __extends = this.__extends || function (d, b) {
+    for (var p in b) if (b.hasOwnProperty(p)) d[p] = b[p];
+    function __() { this.constructor = d; }
+    __.prototype = b.prototype;
+    d.prototype = new __();
+};
 var demo;
 (function (demo) {
-    var vendor;
-    var Translate2DBunny = (function () {
-        function Translate2DBunny() {
+    var Canvas2DBunny = (function () {
+        function Canvas2DBunny() {
             var _this = this;
             /** 重力 */
             this.gravity = 0.5;
@@ -12,41 +19,67 @@ var demo;
             /** bunnyの最小位置Y */
             this.minY = 0;
             /** bunnyの最大位置X */
-            this.maxX = 960 - 26;
+            this.maxX = 960;
             /** bunnyの最大位置Y */
-            this.maxY = 540 - 37;
+            this.maxY = 540;
             /** bunnyの配列 */
             this.bunnySet = [];
-            vendor = (/webkit/i).test(navigator.appVersion) ? 'webkit' : (/firefox/i).test(navigator.userAgent) ? 'Moz' : 'opera' in window ? 'O' : '';
-            this.myDiv = document.getElementById("myDiv");
+            this.canvas = document.getElementById("myCanvas");
+            this.stage = new createjs.Stage(this.canvas);
             this.fps = new FPSChecker();
             this.counter = document.getElementById("counter");
             this.startButton = document.getElementById("startButton");
             this.startButton.addEventListener("click", function () { return _this.startTicker(); });
-            this.startButton.className = "on";
+            this.preloadImage();
         }
-        Translate2DBunny.prototype.startTicker = function () {
-            this.startButton.className = "";
-            this.tick();
-        };
-        Translate2DBunny.prototype.tick = function () {
+        /**
+         * 画像のプリロード
+         * */
+        Canvas2DBunny.prototype.preloadImage = function () {
             var _this = this;
+            this.loadQueue = new createjs.LoadQueue(false);
+            this.loadQueue.addEventListener("complete", function (event) { return _this.loadCompleteHandler(event); });
+            this.loadQueue.loadManifest([
+                { src: "../common/images/bunny.png", id: "bunny" },
+            ], false);
+            this.loadQueue.load();
+        };
+        /**
+         * プリロード完了
+         * */
+        Canvas2DBunny.prototype.loadCompleteHandler = function (event) {
+            var bunnyImage = this.loadQueue.getResult("bunny");
+            this.spriteSheet = new createjs.SpriteSheet({
+                images: [bunnyImage],
+                frames: { width: 26, height: 37, regX: +13, regY: +14 }
+            });
+            this.startButton.className = "on";
+        };
+        Canvas2DBunny.prototype.startTicker = function () {
+            var _this = this;
+            this.startButton.className = "";
+            createjs.Ticker.setFPS(60);
+            createjs.Ticker.timingMode = createjs.Ticker.RAF;
+            createjs.Ticker.addEventListener("tick", function (event) { return _this.tick(event); });
+        };
+        Canvas2DBunny.prototype.tick = function (event) {
             this.fps.finish();
             this.counter.innerHTML = this.bunnySet.length + " BUNNYS, " + this.fps.getFPSText();
             if (this.fps.calculated() && this.fps.getMostRecentFrameRate() <= 30) {
                 alert(this.bunnySet.length + " BUNNY!");
+                createjs.Ticker.removeAllEventListeners("tick");
                 return;
             }
-            var amount = this.fps.fps >= 40 ? 2 : 1;
+            var amount = this.fps.fps >= 40 ? 5 : 1;
             for (var i = 0; i < amount; i++) {
-                var bunny = new Bunny();
+                var bunny = new Bunny(this.spriteSheet);
                 bunny.speedX = Math.random() * 10;
                 bunny.speedY = (Math.random() * 10) - 5;
                 bunny.alpha = 0.3 + Math.random() * 0.7;
                 this.bunnySet.push(bunny);
-                bunny.scale = 0.5 + Math.random() * 0.5;
+                bunny.scaleX = bunny.scaleY = 0.5 + Math.random() * 0.5;
                 bunny.rotation = (Math.random() - 0.5);
-                this.myDiv.appendChild(bunny.image);
+                this.stage.addChild(bunny);
             }
             this.fps.begin();
             for (var i = 0; i < this.bunnySet.length; i++) {
@@ -76,34 +109,28 @@ var demo;
                 }
                 bunny.updatePosition();
             }
-            window.requestAnimationFrame(function () { return _this.tick(); });
+            this.stage.update();
         };
-        return Translate2DBunny;
+        return Canvas2DBunny;
     })();
-    demo.Translate2DBunny = Translate2DBunny;
-    var Bunny = (function () {
+    demo.Canvas2DBunny = Canvas2DBunny;
+    var Bunny = (function (_super) {
+        __extends(Bunny, _super);
         function Bunny() {
+            _super.apply(this, arguments);
             this.positionX = 0;
             this.positionY = 0;
             this.speedX = 0;
             this.speedY = 0;
-            this.rotation = 0;
-            this.alpha = 0;
-            this.scale = 0;
-            this.image = document.createElement("img");
-            this.image.style.position = "absolute";
-            this.image.style.left = "0";
-            this.image.style.top = "0";
-            this.image.src = "../common/images/bunny.png";
         }
         Bunny.prototype.updatePosition = function () {
-            this.image.style[vendor + 'Transform'] = "translate(" + this.positionX + "px, " + this.positionY + "px)" + " rotate(" + this.rotation + "deg) scale(" + this.scale + ")";
-            this.image.style.opacity = this.alpha.toString();
+            this.x = this.positionX;
+            this.y = this.positionY;
         };
         return Bunny;
-    })();
+    })(createjs.Sprite);
 })(demo || (demo = {}));
 window.addEventListener("load", function (event) {
-    new demo.Translate2DBunny();
+    new demo.Canvas2DBunny();
 });
-//# sourceMappingURL=translate2d.js.map
+//# sourceMappingURL=context2d.js.map
